d2356a279954813bb2f03ca900fb8fc97583d91f
MaxiLiria <maaxiliria@gmail.com>
2023-12-13 09:56:27 +0100

    portfolio

--- components/WorkCard/index.js
+++ components/WorkCard/index.js
@@ -8,15 +8,15 @@ const WorkCard = ({ img, name, description, onClick }) => {
     >
       <div
         className="relative rounded-lg overflow-hidden transition-all ease-out duration-300 h-48 mob:h-auto"
-        style={{ height: "600px" }}
+        style={{ height: "450px" }}
       >
         <img
           alt={name}
-          className="h-full w-full object-cover hover:scale-110 transition-all ease-out duration-300"
+          className="h-full w-full object-contain hover:scale-110 transition-all ease-out duration-300"
           src={img}
         ></img>
       </div>
-      <h1 className="mt-5 text-3xl font-medium">
+      <h1 className=" text-3xl font-medium">
         {name ? name : "Project Name"}
       </h1>
       <h2 className="text-xl opacity-50">
